e201566cca82ce90e0a0096abf08c3d944fa4156
Lindsay Potrue <lindsaypotrue@comcast.net>
2017-11-10 12:46:12 -0800

    pics

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ var app = express();
 
 //app.use('/fileuploads', express.static(path.join(__dirname, '/fileuploads')));
 
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
